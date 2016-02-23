1d6afa913cd578aef80b5057e6e8e6ca72cc1cd4
JD Pagano <jonathanpagano@gmail.com>
2016-02-22 23:46:03 -0500

    Refactor string util to handle null input

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,10 +1,18 @@
 export default {
   titleize(string) {
+    if (!string) {
+      return;
+    }
+
     return string.split('_').map(_string => {
       return this.capitalize(_string);
     }).join(' ');
   },
   capitalize(string) {
+    if (!string) {
+      return;
+    }
+
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 };
