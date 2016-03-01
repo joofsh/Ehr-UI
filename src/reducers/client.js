5c08aea9f1126db0c1797d3e9ce463cf42390625
JD Pagano <jonathanpagano@gmail.com>
2016-02-29 20:32:55 -0500

    Fix bug when submitting client questions

--- src/reducers/client.js
+++ src/reducers/client.js
@@ -64,7 +64,6 @@ export default function reducer(state = initialState, action = {}) {
         submitting: false,
         selectedChoiceId: null,
         responses: state.responses.concat(action.response.response),
-        resources: action.response.resources,
         hasAnsweredAllQuestions: !!nextQuestionId,
         currentQuestionId: nextQuestionId
       };
