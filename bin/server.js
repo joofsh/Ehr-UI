f4a1571afbec050a8c8a216bafd9c19a8d56d105
JD Pagano <jonathanpagano@gmail.com>
2017-01-15 20:01:45 -0500

    Fix prod config

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
-require('../server/server.babel');
-var path = require('path');
-var rootDir = path.resolve(__dirname, '..');
-
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
 global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING
 global.__DISABLE_CSR__ = false;  // <----- DISABLES CLIENT SIDE RENDERING
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 global.__TEST__ = process.env.NODE_ENV == 'test';
+
+if (__DEVELOPMENT__) {
+  require('dotenv').config();
+}
+
+require('../server/server.babel');
+var path = require('path');
+var rootDir = path.resolve(__dirname, '..');
+
 
 if (__DEVELOPMENT__) {
   if (!require('piping')({
